3f88775a8c1896c2e6346edc71ecb24c8cf2157c
ethanvachon <ethanhvachon@gmail.com>
2021-07-03 17:15:37 -0600

    service logic / no auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,44 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { api } from "../services/AxiosService";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    profile: {},
     posts: [],
     comments: [],
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    setPosts(state, posts) {
+      state.posts = posts;
+    },
+    setComments(state, comments) {
+      state.comments = comments;
+    },
+    addPost(state, post) {
+      state.posts.push(post);
+    },
+  },
+  actions: {
+    async getPosts({ commit }) {
+      try {
+        let res = await api.get("api/posts");
+        console.log(res.data);
+        commit("setPosts", res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getCommentsByPost({ commit }, id) {
+      let res = await api.get("api/posts/" + id + "/comments");
+      commit("setComments", res.data);
+    },
+    async createPost({ commit }, newPost) {
+      let res = await api.post("api/posts", newPost);
+      commit("addPost", res.data);
+    },
+  },
   modules: {},
 });
