191d0f91a10cea8d04625f184dadc56180848ed9
ethanvachon <ethanhvachon@gmail.com>
2021-07-10 14:45:42 -0600

    comment basic

--- src/components/post.vue
+++ src/components/post.vue
@@ -5,18 +5,33 @@
         <p class="card-text">{{ post.creator.name }}</p>
         <p class="card-text">{{ post.time }}</p>
       </div>
-      <h6 class="p-2">{{ post.body }}</h6>
+      <h6 class="p-2 m-0">{{ post.body }}</h6>
+      <div class="d-flex justify-content-end align-items-center">
+        <p class="pb-2 m-0">{{ comments.length }}</p>
+        <i class="fas fa-comment px-2 pb-2"></i>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { api } from "../services/AxiosService";
 export default {
   props: {
     post: {
       type: Object,
       required: true,
     },
+  },
+  data() {
+    return {
+      comments: null,
+    };
+  },
+  async mounted() {
+    const res = await api.get("api/posts/" + this.post.id + "/comments");
+    this.comments = res.data;
+    console.log(this.comments);
   },
 };
 </script>
@@ -29,5 +44,11 @@ export default {
   font-size: 0.8em;
   padding: 0.5em;
   margin: 0px;
+}
+.fas {
+  -webkit-transform: rotateY(180deg);
+  -moz-transform: rotateY(180deg);
+  -o-transform: rotateY(180deg);
+  -ms-transform: rotateY(180deg);
 }
 </style>
