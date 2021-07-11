658a3f03c3b659ed4d72089c9fc4d1b2dbdeadee
ethanvachon <ethanhvachon@gmail.com>
2021-07-11 17:24:00 -0600

    comment styliong

--- src/components/post.vue
+++ src/components/post.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="d-flex justify-content-center align-items-center">
-    <div class="card width">
+    <div
+      class="card transition"
+      :class="{ size: active == false, 'size-2': active == true }"
+    >
       <div class="d-flex justify-content-between border-bottom">
         <p class="card-text">{{ post.creator.name }}</p>
         <p class="card-text">{{ post.time }}</p>
       </div>
       <h6 class="p-2 m-0">{{ post.body }}</h6>
       <div
-        class="d-flex justify-content-end align-items-center"
+        class="d-flex justify-content-end align-items-center width-100"
         @click="active = !active"
       >
-        <p class="pb-2 m-0">{{ comments != null ? comments.length : 0 }}</p>
-        <i class="fas fa-comment px-2 pb-2"></i>
+        <div class="d-flex align-items-center">
+          <p class="pb-2 m-0">{{ comments != null ? comments.length : 0 }}</p>
+          <i class="fas fa-comment px-2 pb-2"></i>
+        </div>
       </div>
-      <div v-if="active == true" class="d-flex flex-column">
-        <div v-for="comment in comments" :key="comment.id">
-          {{ comment.body }}
+      <div v-if="active == true" class="d-flex flex-column height">
+        <div
+          v-for="comment in comments"
+          :key="comment.id"
+          class="overflow-y-scroll height"
+        >
+          <div class="d-flex flex-column justify-content-center p-1 border-top">
+            <p class="small m-0">{{ comment.creator.name }}</p>
+            <p class="medium m-0">{{ comment.body }}</p>
+          </div>
         </div>
         <form
           @submit.prevent="postComment"
@@ -79,8 +91,22 @@ export default {
 </script>
 
 <style scoped>
-.width {
+.transition {
+  transition: all 0.5s;
+}
+.height {
+  height: 100%;
+}
+.size {
   width: 80%;
+  height: 6rem;
+}
+.size-2 {
+  width: 80%;
+  height: 15rem;
+}
+.overflow-y-scroll {
+  overflow-y: scroll;
 }
 .width-100 {
   width: 100%;
@@ -89,6 +115,9 @@ export default {
   font-size: 0.8em;
   padding: 0.5em;
   margin: 0px;
+}
+.small {
+  font-size: 0.7em;
 }
 .fas {
   -webkit-transform: rotateY(180deg);
